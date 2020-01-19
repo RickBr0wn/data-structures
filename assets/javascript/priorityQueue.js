aba3ed9b479a57853aff4f0445f54df5e516e36c
Rick Brown <ricky.brown.00@gmail.com>
2020-01-19 21:17:45 +0000

    adds priorityQueue

--- assets/javascript/priorityQueue.js
+++ assets/javascript/priorityQueue.js
@@ -1,18 +1,28 @@
 import { createQueue } from './queue'
 
-export function priorityQueue() {
+/**
+ * A simple priority queue allows the user to add items to the queue
+ * that are deemed to be high priority, and get moved ahead in the line.
+ */
+export function createPriorityQueue() {
   const highPriority = createQueue()
   const lowPriority = createQueue()
 
   return {
     add: (item, isHighPriority = false) =>
-      highPriority ? highPriority.add(item) : lowPriority.add(item),
+      isHighPriority ? highPriority.add(item) : lowPriority.add(item),
     remove: () =>
       !highPriority.isEmpty() ? highPriority.remove() : lowPriority.remove(),
-    view: () => [...highPriority, ...lowPriority],
+    view: () =>
+      highPriority.view() === undefined
+        ? lowPriority.view() || []
+        : highPriority
+            .view()
+            .concat(lowPriority.view() || [])
+            .filter(item => item !== undefined),
     peek: () =>
       !highPriority.isEmpty() ? highPriority.peek() : lowPriority.peek(),
-    isEmpty: () =>
-      highPriority.isEmpty() && lowPriority.isEmpty() ? true : false,
+    isEmpty: () => highPriority.isEmpty() && lowPriority.isEmpty(),
+    length: () => highPriority.length() || 0 + lowPriority.length() || 0,
   }
 }
