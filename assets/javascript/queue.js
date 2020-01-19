84736431e43eb866b9e589399c982d37ae1891ad
Rick Brown <ricky.brown.00@gmail.com>
2020-01-19 20:36:08 +0000

    adds checking for array length of zero

--- assets/javascript/queue.js
+++ assets/javascript/queue.js
@@ -8,10 +8,10 @@ export function createQueue() {
   const queue = []
   return {
     add: item => queue.unshift(item),
-    remove: () => queue.pop(),
-    view: () => queue,
-    peek: () => queue[0],
+    remove: () => (queue.length === 0 ? undefined : queue.pop()),
+    view: () => (queue.length === 0 ? undefined : queue),
+    peek: () => (queue.length === 0 ? undefined : queue[queue.length - 1]),
     length: () => queue.length,
-    isEmpty: () => (queue.length === 0 ? true : false)
+    isEmpty: () => queue.length === 0,
   }
 }
