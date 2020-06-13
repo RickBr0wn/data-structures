56b8184fa2565530be89b3fa89abaa40d7b81a3b
Rick Brown <ricky.brown.00@gmail.com>
2020-06-13 01:33:06 +0100

    adds bubbleSort

---
+++ Data_Structures/bubbleSort.js
@@ -0,0 +1,16 @@
+const arr = [6, 4, 5, 3, 2, 1]
+
+const BubbleSort = function (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+console.log(BubbleSort(arr))
