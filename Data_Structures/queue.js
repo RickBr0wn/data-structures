7042bd9f16bd99daf4a3d7d179d4f69f1e3617c7
Rick Brown <ricky.brown.00@gmail.com>
2020-11-11 01:53:26 +0000

    remove whitespace

--- Data_Structures/queue.js
+++ Data_Structures/queue.js
@@ -8,15 +8,10 @@ export function createQueue() {
   const queue = []
   return {
     add: (item) => queue.unshift(item),
-
     remove: () => (queue.length === 0 ? undefined : queue.pop()),
-
     view: () => (queue.length === 0 ? undefined : queue),
-
     peek: () => (queue.length === 0 ? undefined : queue[queue.length - 1]),
-
     length: () => queue.length,
-
     isEmpty: () => queue.length === 0,
   }
 }
